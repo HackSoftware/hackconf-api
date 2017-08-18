eb25cb6b051fc65efbb289bbb9cad9e2343ece90
Plamen Neshkov <plamen@tuta.io>
2017-08-18 14:42:16 +0300

    "Upgrade" schedule

--- app/controllers/ScheduleController.scala
+++ app/controllers/ScheduleController.scala
@@ -2,12 +2,13 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import models.Schedule
+import models.{Schedule, ScheduleRequest}
+import play.api.libs.json.{JsArray, JsObject}
 import play.api.libs.json.Json.toJson
 import play.api.mvc.{AbstractController, ControllerComponents}
 import repositories.ScheduleRepository
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class ScheduleController @Inject() (
@@ -15,12 +16,26 @@ class ScheduleController @Inject() (
   schedule: ScheduleRepository
 )(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
-  def index(year: Int, month: Int, day: Int) = Action.async {
-    schedule.get(year, month, day).map { x =>
-      val items = x.map {
-        case ((talkSchedule, talk), speaker) => (talkSchedule, talk, speaker)
-      }.toList
-      Schedule(items)
-    }.map(x => Ok(toJson(x)))
+  def get = Action.async(parse.json[ScheduleRequest]) { implicit req =>
+    Future.sequence(
+      // Don't ask...
+      req.body.dates.map { date =>
+        schedule.get(date.getYear, date.getMonthOfYear, date.getDayOfMonth).map { x =>
+          val items = x.map {
+            case ((talkSchedule, talk), speaker) => (talkSchedule, talk, speaker)
+          }.toList
+
+          date -> Schedule(items)
+        }
+      }
+      // Don't ask...
+    ).map(_.toMap).map {
+      x => Ok(JsArray(
+        x.map { case (d, s) =>
+          JsObject(Map(d.toString("yyyy-MM-dd") -> toJson(s)))
+        }.toSeq
+      ))
+    }
   }
+
 }
